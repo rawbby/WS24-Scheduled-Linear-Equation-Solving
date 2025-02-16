3eb33537e1d15f23602ebab7919866853ec60fb1
Robert Andreas Fritsch <git@robert-fritsch.de>
2025-02-16 15:49:20 +0100

    transfer for bench

--- bench_online_scheduling/bench.sh
+++ bench_online_scheduling/bench.sh
@@ -1,3 +1,14 @@
 # sudo cpupower frequency-set -g userspace
 # sudo cpupower frequency-set -d 2.0GHz -u 2.0GHz
-# sudo chrt -f 99 perf stat -e cache-misses,branch-misses,cycles,instructions ./bench_online_scheduling
+
+#scheduler_name="trivial"
+num_threads=6
+load_factor="600.0"
+min_n=32
+max_n=1024
+score="200.0"
+
+cd ../cmake-build-release/bin
+sudo chrt -f 99 perf stat -e cache-misses,branch-misses,cycles,instructions ./bench_online_scheduling "trivial" $num_threads $load_factor $min_n $max_n $score
+sudo chrt -f 99 perf stat -e cache-misses,branch-misses,cycles,instructions ./bench_online_scheduling "parallel" $num_threads $load_factor $min_n $max_n $score
+sudo chrt -f 99 perf stat -e cache-misses,branch-misses,cycles,instructions ./bench_online_scheduling "mixed" $num_threads $load_factor $min_n $max_n $score
